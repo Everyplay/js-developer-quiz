b91623f62b679bcf819d17c08218809ed65a2479
Mikko Lehtinen <mikko@Mikkos-Applifier-MacBook-Pro-296.local>
2014-08-27 09:14:02 +0300

    better description

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ exports.kebabify = function(str) {
 };
 
 /**
- * Read titles & urls of hits from Google. It should read titles of pages for given urls
- * from Google that are not hosted in everyplay.com or unity3d.com. If there is multiple
- * urls, read result in parallel from Google. Use bluebird for Promises implemention.
+ * Read titles & urls of hits from Google. It should omit pages
+ * hosted in everyplay.com or unity3d.com (or their subdomains). If there is multiple
+ * urls, read results in parallel. Use bluebird for Promises implemention.
  *
  * @param  {Array of Strings} urls to crawl
  * @return {Promise} it should resolve a Promise containing titles and urls of pages
